b7f074a2fff173dda9a1f05a3ad28947682ea574
Helan Mariyam Aipe <89463235+Helan23@users.noreply.github.com>
2024-09-05 18:10:26 +0530

    Add files via upload

---
+++ TASK 6/frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+export default function Footer() {
+    
+    return (
+        <div style={{padding: "12px 0px", backgroundColor : "rgb(255, 0, 0)", textAlign: 'center', position:'fixed', width: '100%',height : '85px', bottom : '0', left: '0', right: '0', zIndex : '999'}}>
+            
+            <div className="text-center">
+                    <h5>
+                        Copyright : Helan
+                    </h5>
+            </div> 
+
+        </div>
+       
+    )
+}
